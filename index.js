59a32469c3b700c632f454641e3d6d5919ecda2d
CGoddard1 <goddard.cole@gmail.com>
2023-04-25 18:57:39 -0400

    added arrays

--- index.js
+++ index.js
@@ -22,3 +22,45 @@ async function homePrompt() {
         ],
     });
     userResponse = answers.homePrompt;
+
+    switch (userResponse) {
+        case "View All Departments":
+            const deptRes = await promptFunctions.getAllDepartments();
+            console.table(deptRes[0]);
+            homePrompt();
+            break;
+        case "View All Roles":
+            const empRoleRes = await promptFunctions.getAllRoles();
+            console.table(empRoleRes[0]);
+            homePrompt();
+            break;
+        case "View All Employees":
+            const empRes = await promptFunctions.getAllEmployees();
+            console.table(empRes[0]);
+            homePrompt();
+            break;
+        case "Add a Department":
+            await promptFunctions.addDepartment();
+            homePrompt();
+            break;
+        case "Add a Role":
+            await promptFunctions.addRole();
+            homePrompt();
+            break;
+        case "Add an Employee":
+            await promptFunctions.addEmployee();
+            homePrompt();
+            break;
+        case "Update an Employee Role":
+            await promptFunctions.updateEmployeeRole();
+            homePrompt();
+            break;
+        case "Quit":
+            process.exit();
+        default:
+            console.log("Invalid selection. Please try again.");
+            homePrompt();
+    };
+};
+
+homePrompt();

81a2c1776552178cafea28634335ff241fa1284e
CGoddard1 <goddard.cole@gmail.com>
2023-04-25 18:13:28 -0400

    added function

--- index.js
+++ index.js
@@ -4,3 +4,21 @@ const inquirer = require("inquirer");
 const promptFunctions = require("./utils/prompts")
 require("console.table");
 
+//function
+async function homePrompt() {
+    const answers = await inquirer.prompt({
+        name: "homePrompt",
+        type: "list",
+        message: "Please select an option from the list.",
+        choices: [
+            "View All Departments",
+            "View All Roles",
+            "View All Employees",
+            "Add a Department",
+            "Add a Role",
+            "Add an Employee",
+            "Update an Employee Role",
+            "Quit",
+        ],
+    });
+    userResponse = answers.homePrompt;
